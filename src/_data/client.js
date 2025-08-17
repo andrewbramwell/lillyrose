3596a0058db2437b1741e744e902406c7c69a23e
andrewbramwell <andy@voucherstore.io>
2025-08-17 12:06:36 +0100

    first commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "name": "Splendid Sites Web Designs",
-    "email": "help@splendidsites.com",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Lily and Rose Thai Massage and Retreat Centre",
+    "email": "",
+    "phoneForTel": "+447780475640",
+    "phoneFormatted": "07780 475640",
     "openingHours": "Mon-Sun: 9am-9pm",
-    "onlineBooking": "https://www.facebook.com/",
+    "onlineBooking": "https://lilyandrosethaimassage.setmore.com/",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "lineOne": "Aaron Hill Farm",
+        "lineTwo": "Cragg Vale",
+        "city": "Hebden Bridge",
+        "state": "",
+        "zip": "HX7 5TX",
+        "country": "UK",
+        "mapLink": "https://maps.app.goo.gl/oMwbkapazBG1ma4d9"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
